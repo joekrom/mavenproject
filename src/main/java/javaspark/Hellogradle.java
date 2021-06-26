87185b449b52f2060557d4b37fe4f3e45a230bfb
joekrom <joel.dfankam@yahoo.fr>
2021-06-26 10:11:52 +0200

    Heroku dependencies and function to get the right port on the heroku cloud

--- src/main/java/javaspark/Hellogradle.java
+++ src/main/java/javaspark/Hellogradle.java
@@ -9,5 +9,13 @@ public class Hellogradle {
 		get("/hello", (req, res) -> "Hello world: microapp with maven complete");
 
 	}
+	
+	static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	}
 
 }
